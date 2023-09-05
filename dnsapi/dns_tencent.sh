18d107950983cc4a8464c2201ec13ec08416ba50
KincaidYang <91786638+KincaidYang@users.noreply.github.com>
2023-09-05 23:46:26 +0800

    Update dns_tencent.sh

--- dnsapi/dns_tencent.sh
+++ dnsapi/dns_tencent.sh
@@ -150,7 +150,7 @@ tencent_sha256() {
 tencent_hmac_sha256() {
   k=$1
   shift
-  hex_key="$(printf %b "$k" | _hex_dump | tr -d " ")" | _base64 | tr -- '+/' '-_')"
+  hex_key="$(printf %b "$k" | _hex_dump | tr -d " ")"
   printf %b "$@" | _hmac sha256 "$hex_key" hex
 }
 
